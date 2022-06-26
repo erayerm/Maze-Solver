487353773a62d3fccc8221fdd8c840911ce61fca
erayerm <ermemiseray@gmail.com>
2022-06-26 16:59:57 +0300

    Update main.cpp
    New function for creating matrix. New function for showing the result on console

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <windows.h>
 
 using namespace std;
 
@@ -19,77 +20,16 @@ vector<int> movementColumn;
 
 void        move(int, int);
 void        control(int, int);
+void        setconsolecolor(int, int);
+void        show();
+void        creatingMatrix();
 
 int main()
 {
-
-	//in matrix;
-	//0 for empty roads
-	//1 for walls
-	//2 for right way
-	
-    for (int i = 0; i < lineCount; i++)
-    {
-        for (int j = 0; j < columnCount; j++)
-        {
-            matrix[i][j] = 0;
-        }
-    }
-
-    matrix[1][0] = 1;
-    matrix[2][0] = 1;
-    matrix[3][0] = 1;
-    matrix[4][0] = 1;
-    matrix[5][0] = 1;
-    matrix[6][0] = 1;
-
-    matrix[8][0] = 1;
-
-    matrix[1][1] = 1;
-    matrix[3][1] = 1;
-
-    matrix[3][2] = 1;
-    matrix[4][2] = 1;
-    matrix[5][2] = 1;
-    matrix[6][2] = 1;
-    matrix[8][2] = 1;
-
-    matrix[0][3] = 1;
-    matrix[1][3] = 1;
-    matrix[3][3] = 1;
-    matrix[8][3] = 1;
-
-    matrix[3][4] = 1;
-    matrix[5][4] = 1;
-    matrix[6][4] = 1;
-    matrix[7][4] = 1;
-    matrix[8][4] = 1;
-
-    matrix[1][5] = 1;
-    matrix[8][5] = 1;
-
-    matrix[0][6] = 1;
-    matrix[1][6] = 1;
-    matrix[2][6] = 1;
-    matrix[3][6] = 1;
-    matrix[5][6] = 1;
-    matrix[7][6] = 1;
-    matrix[8][6] = 1;
-
-    matrix[0][7] = 1;
-    matrix[5][7] = 1;
-
-    matrix[0][8] = 1;
-    matrix[1][8] = 1;
-    matrix[2][8] = 1;
-    matrix[3][8] = 1;
-    matrix[4][8] = 1;
-    matrix[5][8] = 1;
-    matrix[6][8] = 1;
-    matrix[7][8] = 1;
-
+	creatingMatrix();
     move(start_line_coordinate, start_column_coordinate);
-
+	show();
+	
     return 0;
 }
 
@@ -217,4 +157,105 @@ void control(int start_line, int start_column) {
     if (start_line == finish_line && start_column == finish_column) {
         isEnd = true;
     }
+}
+
+//this function is for showing the solution on console
+void show() {
+    for (int i = 0; i < lineCount; i++)
+    {
+        for (int j = 0; j < columnCount; j++)
+        {
+			switch(matrix[i][j]){
+				case 0:
+					setconsolecolor(0, 8);
+					cout << "  ";
+					break;
+				case 1:
+					setconsolecolor(0, 4);
+					cout << "  ";
+					break;
+				case 2:
+					setconsolecolor(0, 9);
+					cout << "  ";
+					break;
+			}
+        }
+		cout << endl;
+        setconsolecolor(0, 0);
+    }
+}
+
+//creating maze in this function
+void creatingMatrix (){
+	
+	//0 for empty roads
+	//1 for walls
+	//2 for right way
+	
+    for (int i = 0; i < lineCount; i++)
+    {
+        for (int j = 0; j < columnCount; j++)
+        {
+            matrix[i][j] = 0;
+        }
+    }
+
+    matrix[1][0] = 1;
+    matrix[2][0] = 1;
+    matrix[3][0] = 1;
+    matrix[4][0] = 1;
+    matrix[5][0] = 1;
+    matrix[6][0] = 1;
+
+    matrix[8][0] = 1;
+
+    matrix[1][1] = 1;
+    matrix[3][1] = 1;
+
+    matrix[3][2] = 1;
+    matrix[4][2] = 1;
+    matrix[5][2] = 1;
+    matrix[6][2] = 1;
+    matrix[8][2] = 1;
+
+    matrix[0][3] = 1;
+    matrix[1][3] = 1;
+    matrix[3][3] = 1;
+    matrix[8][3] = 1;
+
+    matrix[3][4] = 1;
+    matrix[5][4] = 1;
+    matrix[6][4] = 1;
+    matrix[7][4] = 1;
+    matrix[8][4] = 1;
+
+    matrix[1][5] = 1;
+    matrix[8][5] = 1;
+
+    matrix[0][6] = 1;
+    matrix[1][6] = 1;
+    matrix[2][6] = 1;
+    matrix[3][6] = 1;
+    matrix[5][6] = 1;
+    matrix[7][6] = 1;
+    matrix[8][6] = 1;
+
+    matrix[0][7] = 1;
+    matrix[5][7] = 1;
+
+    matrix[0][8] = 1;
+    matrix[1][8] = 1;
+    matrix[2][8] = 1;
+    matrix[3][8] = 1;
+    matrix[4][8] = 1;
+    matrix[5][8] = 1;
+    matrix[6][8] = 1;
+    matrix[7][8] = 1;
+}
+
+//this function is for changing text and background color at windows console
+void setconsolecolor(int textColor, int bgColor)
+{
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), (textColor +
+        (bgColor * 16)));
 }
